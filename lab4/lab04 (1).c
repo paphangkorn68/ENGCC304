8dffa4fbefc4e159e8e89a53377ef83f1be5d2f1
Paphangkorn <paphangkorn.xp@gmail.com>
2025-07-25 11:03:28 +0700

    Add files via upload

---
+++ lab4/lab04 (1).c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    char Id[10] ;
+    int hrs = 0 ;
+    int salary_hr = 0 ;
+    float salary = 0 ;
+    
+    printf( "Employees_ID(Max. 10 chars)\n") ;
+    scanf( "%10s" , Id ) ;
+    
+    printf( "The_working_hrs\n") ;
+    scanf( "%d" , &hrs ) ;
+    
+    printf( "Salary_amout/hr\n") ;
+    scanf( "%d" , &salary_hr ) ;
+    
+    salary = salary_hr * hrs ;
+    
+    printf("Employees_ID = %s \n" , Id );
+    printf("Salary = %.2f " , salary );
+    
+} //end function
+ 
