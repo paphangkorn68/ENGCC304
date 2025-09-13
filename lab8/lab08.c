ae7ac72c58b44038420527d0fdb72c0e95eee134
Paphangkorn <paphangkorn.xp@gmail.com>
2025-09-12 23:16:07 -0700

    Add files via upload

--- lab8/lab08.c
+++ lab8/lab08.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    int sec, munny, munny1, munny2, i, pj, y ;
-    float x, bonus;
-    do {
+
+    int sec, munny, munny1, munny2, i, pj, y ;                  //ประกาศตัวแปล รับค่า จำนวนเต็ม
+    float x, bonus;                                             //ประกาศตัวแปล รับค่า จำนวนทศนิยม
+
+    do {                                                        // Do while 1 แสดงการรับค่าของตำแหน่งผู้ใช้งาน เพื่อตรวจสอบว่าตำแหน่งไหนมีเงินเท่าไหล่
         printf( "What is your position? \n" ) ;
         printf( "Junior Programmer    = 1 \n" ) ;
         printf( "Mid-Level Programmer = 2\n" ) ;
         printf( "Senior Programmer    = 3 \n" ) ;
-        if( scanf( "%d", &sec ) != 1 ) {
+        if( scanf( "%d", &sec ) != 1 ) {                        //ตรวจสอบว่าเป็นตัวเลขหรือไม่ ถ้าไม่ใช่ ให้ทำการแสดง number only และล้างบัฟเฟอ
             printf( "number only \n" ) ;
             while ( getchar() != '\n' ) ;
-        }else {
+        }else {                                                 
             if( sec == 3 ) {
                 munny = 50000 ;
                 break ;
@@ -22,31 +24,36 @@ int main() {
                 munny = 20000 ;
                 break ;
             }
-        }
-    }while( 1 ) ;
-    i = munny ;
-    do {
+        }                                                       //จบ if
+    }while( 1 ) ;                                               //จบ Do while 1 
+
+    i = munny ;                                                 //กำหนดให้ i = munny เพื่อใช้ในตอนบอก Basic salary
+
+    do {                                                        // Do while 2 ทำหน้าที่รับจำนวนปีที่ทำงาน
         printf( "How many years have you worked? \n" ) ;
-        if( scanf( "%d", &y ) != 1 ) {
+        if( scanf( "%d", &y ) != 1 ) {                          //ตรวจสอบว่าเป็นตัวเลขหรือไม่ ถ้าไม่ใช่ ให้ทำการแสดง number only และล้างบัฟเฟอ
             printf( "number only \n" ) ;
             while ( getchar() != '\n' ) ;
         }else {
             break ;
-        }
-    }while( 1 ) ;
-    do {
-        printf( "How many projects have you completed? \n" ) ;
-        if( scanf( "%d", &pj ) != 1 ) {
+        }                                                       //จบ if
+    }while( 1 ) ;                                               //จบ Do while 2
+
+    do {                                                        // Do while 3 ทำหน้าที่รับจำนวนโปรเจกที่ทำเสร็จไปในปีที่ทำงาน
+        printf( "How many projects have you completed? \n" ) ;  
+        if( scanf( "%d", &pj ) != 1 ) {                         //ตรวจสอบว่าเป็นตัวเลขหรือไม่ ถ้าไม่ใช่ ให้ทำการแสดง number only และล้างบัฟเฟอ
             printf( "number only \n" ) ;
             while ( getchar() != '\n' ) ;
         }else {
             break ;
-        }
-    }while( 1 ) ;
-    if( pj > 5 ) {
+        }                                                       //จบ if
+    }while( 1 ) ;                                               //จบ Do while 3
+
+    if( pj > 5 ) {                                              //ตรวจสอบว่ามีการทำโปรเจกมากกว่า 5 หรือไม่เพื่อเพื่ม Special bonus
         bonus = 0.05 ;
-    }
-    if( y > 5) {
+    }                                                           //จบ if
+
+    if( y > 5) {                                                //รวจสอบว่าทำงานมากี่ปีเพื่อกำหนดตัวคูณ bonus
         x = 0.2 ;
     }else if (y >= 4 ) {
         x = 0.15 ;
@@ -54,14 +61,17 @@ int main() {
         x = 0.1 ;
     }else {
         x = 0 ;
-    }
-    munny1 = munny * x ;
-    munny2 = munny * bonus ;
-    munny = munny + munny1 + munny2 ;
-    printf( "Basic salary = %d \n", i ) ;
+    }                                                           //จบ if
+
+    munny1 = munny * x ;                                        //คำนวน bonus
+    munny2 = munny * bonus ;                                    //คำนวน Special bonus
+    munny = munny + munny1 + munny2 ;                           //คำนวน net salary
+
+    printf( "Basic salary = %d \n", i ) ;                       
     printf( "Bonus based on length of service = %d \n", munny1 ) ;
     printf( "Special bonus = %d \n", munny2 ) ;
-    printf( "net salary = %d\n", munny ) ;
+    printf( "net salary = %d \n", munny ) ;
     printf( "_____________________________________________________________________- \n" ) ;
+
     return 0 ; 
-}
+}                                                                  //จบ main
